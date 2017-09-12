d0ed6aad0e4ce35735c6da36a1e3b830c316a635
David <David@DESKTOP-0BQVQUA>
2017-09-12 13:48:03 +0200

    Skapat mbox

--- TestSync1/Form1.cs
+++ TestSync1/Form1.cs
@@ -16,5 +16,10 @@ namespace TestSync1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hej");
+        }
     }
 }
